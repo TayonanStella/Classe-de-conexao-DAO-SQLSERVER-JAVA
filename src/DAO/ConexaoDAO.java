d3042ec399fda6d28b202793e3f707b78b8959b2
TayonanStella <tayonanstella22@hotmail.com>
2024-06-03 20:03:55 -0500

    primeiro commit

---
+++ src/DAO/ConexaoDAO.java
@@ -0,0 +1,30 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class ConexaoDAO {
+    private Connection con;
+
+    public Connection connect() {
+        try {
+            // Define Driver de conexão JDBC thin
+            String URL =  "jdbc:sqlserver://localhost:50833;databaseName=softrestaurant10;user=sa;password=National09";
+ 
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(URL);   
+            
+ 
+            JOptionPane.showMessageDialog(null, "Conexão Efetuada com sucesso!");
+        } catch (SQLException error) {
+            JOptionPane.showMessageDialog(null, "Falha na conexão");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexaoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+}
